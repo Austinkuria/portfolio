93a1d76e5b28ac3cb6443e47cba10882a49104b1
Austinkuria <kuriaaustin125@gmail.com>
2025-06-04 18:30:43 +0300

    Add Google Tag Manager integration for enhanced tracking and analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Providers } from './providers';
 import { GeistSans } from 'geist/font/sans';
 import { GeistMono } from 'geist/font/mono';
 import './globals.css';
+import Script from 'next/script';
 
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
@@ -54,8 +55,22 @@ export default function RootLayout({
             }, true);
           `
         }} />
+        
+        {/* Google Tag Manager */}
+        <Script id="gtm-head" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-P2G8NGPF');`}
+        </Script>
       </head>
       <body className={`${GeistSans.variable} ${GeistMono.variable}`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P2G8NGPF"
+            height="0" width="0" style={{display:'none',visibility:'hidden'}}></iframe>
+        </noscript>
         <Providers>
           <Header />
           <ClientLayoutWrapper>{children}</ClientLayoutWrapper>
